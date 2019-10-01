c7e0e2a19fb21c82d7277d1b44aace68faeda790
Nzubechukwu <nzubennamani@gmail.com>
2019-10-01 20:23:37 +0100

    associate snippets with users update the snippetserializer to reflect the user model association add isauthenticatedorreadonly permission class to the snippetdetails and snippetlist views [finishes]

--- pastebin_project/snippets/serializers.py
+++ pastebin_project/snippets/serializers.py
@@ -3,9 +3,10 @@ from .models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 from django.contrib.auth.models import User
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
     # id = serializers.IntegerField(read_only=True)
     # title = serializers.CharField(required=False, allow_blank=True, max_length=100)
     # code = serializers.CharField(style={'base_template': 'textarea.html'})
